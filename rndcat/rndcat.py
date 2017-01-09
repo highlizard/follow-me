3454420a09b694dd06a7e602f1e974f784ea61b4
highlizard <wizard8lizard@yahoo.com>
2017-01-09 16:08:06 +0900

    Add files via upload

---
+++ rndcat/rndcat.py
@@ -0,0 +1,26 @@
+import discord
+from discord.ext import commands
+from __main__ import send_cmd_help
+import aiohttp
+
+class rndcat:
+    def __init__(self, bot):
+        self.bot = bot
+       
+    async def listener(self, message):
+        channel = message.channel
+        if message.author.id != self.bot.user.id:
+            if message.content.lower().startswith('ร่างกายต้องการแมว') or message.content.lower().startswith('aayy'):
+                search = "http://random.cat/meow"
+                try:
+                     async with aiohttp.get(search) as r:
+                result = await r.json()
+            await self.bot.say(result['file'])
+                except:
+            await self.bot.say("Couldnt Get An Image")
+
+
+def setup(bot):
+    n = rndcat(bot)
+    bot.add_listener(n.listener, "on_message")
+    bot.add_cog(n)
