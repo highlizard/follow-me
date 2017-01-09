c27c7ceeed3f33b73e5eb94f8408fade30236f42
highlizard <wizard8lizard@yahoo.com>
2017-01-09 16:16:42 +0900

    Update rndcat.py

--- rndcat/rndcat.py
+++ rndcat/rndcat.py
@@ -4,23 +4,22 @@ from __main__ import send_cmd_help
 import aiohttp
 
 class rndcat:
+    """Animal commands."""
+
     def __init__(self, bot):
         self.bot = bot
-       
-    async def listener(self, message):
-        channel = message.channel
-        if message.author.id != self.bot.user.id:
-            if message.content.lower().startswith('ccat') or message.content.lower().startswith('aayy'):
-                search = "http://random.cat/meow"
-                try:
-                     async with aiohttp.get(search) as r:
+
+    @commands.command()
+    async def cats(self):
+        """Shows a cat"""
+        search = "http://random.cat/meow"
+        try:
+            async with aiohttp.get(search) as r:
                 result = await r.json()
             await self.bot.say(result['file'])
-                except:
-            await self.bot.say("Couldnt Get An Image")
-
-
+        except:
+            await self.bot.say("Couldn't Get any cat")
+            
 def setup(bot):
     n = rndcat(bot)
-    bot.add_listener(n.listener, "on_message")
     bot.add_cog(n)
