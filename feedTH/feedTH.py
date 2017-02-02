a3ebee49ac750ddb75615773f6b2e708fd191bfb
highlizard <wizard8lizard@yahoo.com>
2017-02-02 17:15:03 +0900

    Update feedTH.py

--- feedTH/feedTH.py
+++ feedTH/feedTH.py
@@ -89,9 +89,7 @@ class FeedTH:
     async def feed(self, user : discord.Member):
         """Force A food Item Down A Users Throat"""
         if user.id == self.bot.user.id:
-            await self.bot.say("เลเวียจะรับ {} จากคุณ {} "
-                               "ค่ะ".format(rndchoice(self.items),
-                                           user.name))
+            await self.bot.say("เลเวียจะรับ {} จากคุณค่ะ".format(rndchoice(self.items)))                                           
             return
         await self.bot.say("- เลเวียเสิร์ฟ {} ให้คุณ {} "
                            " ค่ะ -".format(rndchoice(self.items),
